2f8a30cdda1a2dcbc2bf894143f15d955ed5ea28
ali.ghafouri <ghafooridev@gmail.com>
2025-09-02 18:34:39 +0330

    feat: add test

--- apps/elder-auth/src/app/prisma/prisma.service.spec.ts
+++ apps/elder-auth/src/app/prisma/prisma.service.spec.ts
@@ -1,18 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaService } from './prisma.service';
-
-describe('PrismaService', () => {
-  let service: PrismaService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService],
-    }).compile();
-
-    service = module.get<PrismaService>(PrismaService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+describe('test', () => {
+  it('sum', () => {
+    expect(1 + 1).toBe(2);
   });
 });
