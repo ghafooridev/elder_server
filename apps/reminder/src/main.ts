b81747ccf98269af8333ad104b5e2846ff860055
ali.ghafouri <ghafooridev@gmail.com>
2025-09-07 13:30:47 +0330

    feat: create reminder

---
+++ apps/reminder/src/main.ts
@@ -0,0 +1,47 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from '@elder/nestjs';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
+  app.enableCors({
+    credentials: true,
+    exposedHeaders: ['Set-Cookie'],
+    origin: config.getOrThrow<string>('AUTH_ALLOWED_ORIGIN').split(', '),
+    allowedHeaders: ['Content-Type', 'Origin', 'Accept', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+
+  app.use(cookieParser());
+
+  const globalPrefix = 'api';
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+  app.setGlobalPrefix(globalPrefix);
+  const port = config.getOrThrow('REMINDER_PORT');
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
